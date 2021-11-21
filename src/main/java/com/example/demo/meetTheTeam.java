cabee09ac4a55d3f791cadf6eb4820115c538289
ChiragSuthar0 <sutharchirag128@gmail.com>
2021-11-21 19:40:44 +0530

    commit: Handed over for UI/UX design to Ritesh

--- src/main/java/com/example/demo/meetTheTeam.java
+++ src/main/java/com/example/demo/meetTheTeam.java
@@ -42,4 +42,13 @@ public class meetTheTeam {
         stage.show();
     }
 
+    public void changeToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
 }
