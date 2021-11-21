306c30c17dd3fc7f60d03ac32703552cfce665f3
ChiragSuthar0 <sutharchirag128@gmail.com>
2021-11-21 19:38:33 +0530

    commit: Handed over for UI/UX design to Ritesh

--- src/main/java/com/example/demo/Algorithms.java
+++ src/main/java/com/example/demo/Algorithms.java
@@ -47,6 +47,15 @@ public class Algorithms {
         stage.show();
     }
 
+    public void changeToHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
     @FXML
             private TextField numberOfProcesses;
     @FXML
